a0a4c4392249631819ba67119053c196f615d27c
brooklinsantosh <brooklinsantosh@gmail.com>
2023-10-26 10:13:05 +0530

    Created basic API for prediction

---
+++ app.py
@@ -0,0 +1,28 @@
+import pickle
+from flask import Flask, request, app, jsonify, url_for, render_template
+import numpy as np
+import pandas as pd
+
+app = Flask(__name__)
+
+#Load the model
+model = pickle.load(open('models/reg_model.pkl','rb'))
+scl = pickle.load(open('models/scaler.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api', methods=['POST'])
+def predict_api():
+    data = request.json['data']
+    print(data)
+    df = pd.DataFrame(data, index= [0])
+    scaled_data = scl.transform(df)
+    print(scaled_data)
+    pred = model.predict(scaled_data)
+    print(pred[0])
+    return jsonify(pred[0])
+
+if __name__ == "__main__":
+    app.run(debug=True)
